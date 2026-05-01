c12c52afbf25a61ced7f1968b38291e5d703fce8
Scott <bndrbots@gmail.com>
2026-04-30 23:16:24 -0700

    Add 'all' to allowedHosts in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: 'bndrmix-production.up.railway.app',
+    allowedHosts: 'bndrmix-production.up.railway.app','all'
   },
 })
